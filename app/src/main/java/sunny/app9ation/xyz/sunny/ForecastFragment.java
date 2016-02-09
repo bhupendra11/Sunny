0f5449fae7c00414414740588dfebb4aaf8ccc21
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2016-02-09 23:00:19 +0530

    Add loader, cursor adaptors and leveraged projections . (Mid lesson 4c)

--- app/src/main/java/sunny/app9ation/xyz/sunny/ForecastFragment.java
+++ app/src/main/java/sunny/app9ation/xyz/sunny/ForecastFragment.java
@@ -1,30 +1,62 @@
     package sunny.app9ation.xyz.sunny;
 
-    import android.content.Intent;
-import android.content.SharedPreferences;
+    import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+    import android.support.v4.content.CursorLoader;
+    import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import sunny.app9ation.xyz.sunny.data.WeatherContract;
 
     /**
      * A placeholder fragment containing a simple view.
      */
-    public class ForecastFragment extends Fragment {
+    public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
-        ArrayAdapter<String> mForecastAdapter;
-        ListView listView;
+        ForecastAdapter mForecastAdapter;
+        public static final int FORECAST_LOADER =0;
+
+        private static final String[] FORECAST_COLUMNS = {
+                // In this case the id needs to be fully qualified with a table name, since
+                // the content provider joins the location & weather tables in the background
+                // (both have an _id column)
+                // On the one hand, that's annoying.  On the other, you can search the weather table
+                // using the location set by the user, which is only in the Location table.
+                // So the convenience is worth it.
+                WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+                WeatherContract.WeatherEntry.COLUMN_DATE,
+                WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+                WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+                WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+                WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+                WeatherContract.LocationEntry.COLUMN_COORD_LONG
+        };
+
+        // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+        // must change.
+        static final int COL_WEATHER_ID = 0;
+        static final int COL_WEATHER_DATE = 1;
+        static final int COL_WEATHER_DESC = 2;
+        static final int COL_WEATHER_MAX_TEMP = 3;
+        static final int COL_WEATHER_MIN_TEMP = 4;
+        static final int COL_LOCATION_SETTING = 5;
+        static final int COL_WEATHER_CONDITION_ID = 6;
+        static final int COL_COORD_LAT = 7;
+        static final int COL_COORD_LONG = 8;
 
         public ForecastFragment() {
         }
@@ -53,7 +85,7 @@ import java.util.ArrayList;
         }
 
         private void updateWeather(){
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getContext(),mForecastAdapter);
+        FetchWeatherTask weatherTask = new FetchWeatherTask(getContext());
 
         //get location settings using SharedPrefences
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
@@ -74,34 +106,48 @@ import java.util.ArrayList;
                                  Bundle savedInstanceState) {
             View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
 
-             mForecastAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    //ID of list item layout
-                    R.layout.list_item_forecast,
-                    // ID of the textview to populate
-                    R.id.list_item_forecast_textview,
-                    //arraylist of items
-                   new ArrayList<String>());
+            mForecastAdapter = new ForecastAdapter(getActivity(), null, 0);
+
 
             ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
             listView.setAdapter(mForecastAdapter);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    String forecast = mForecastAdapter.getItem(position);
-                    //Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(getActivity(), DetailActivity.class)
-                            .putExtra(Intent.EXTRA_TEXT,forecast);
-                    startActivity(intent);
-
-                }
-            });
 
 
             return rootView;
         }
 
 
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            getLoaderManager().initLoader(FORECAST_LOADER, null,this);
+        }
 
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            String locationSetting = Utility.getPreferredLocation(getActivity());
+            String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+            Uri weatherForLocationUri = WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(
+                    locationSetting, System.currentTimeMillis());
+
+
+            return new CursorLoader(
+                    getActivity(),
+                    weatherForLocationUri,
+                    FORECAST_COLUMNS,
+                    null,
+                    null,
+                    sortOrder);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+            mForecastAdapter.swapCursor(cursor);
+        }
+
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            mForecastAdapter.swapCursor(null);
+        }
     }
