2ccfc97906f72c59c943c75c76ee29b0dc98dd4f
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2016-04-07 02:23:48 +0530

    change icon pack based on user preference

--- app/src/main/java/sunny/app9ation/xyz/sunny/SettingsActivity.java
+++ app/src/main/java/sunny/app9ation/xyz/sunny/SettingsActivity.java
@@ -36,6 +36,7 @@ public class SettingsActivity extends PreferenceActivity
         // updated when the preference changes.
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
         bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_temperature_units_key)));
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_art_pack_key)));
     }
 
 
@@ -117,6 +118,15 @@ public class SettingsActivity extends PreferenceActivity
         }
         else if(key.equals(getString(R.string.pref_units_key))){
                 getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI,null);
+        }
+        else if ( key.equals(getString(R.string.pref_location_status_key)) ) {
+            // our location status has changed.  Update the summary accordingly
+            Preference locationPreference = findPreference(getString(R.string.pref_location_key));
+            bindPreferenceSummaryToValue(locationPreference);
+        }
+        else if ( key.equals(getString(R.string.pref_art_pack_key)) ) {
+            // art pack have changed. update lists of weather entries accordingly
+            getContentResolver().notifyChange(WeatherContract.WeatherEntry.CONTENT_URI, null);
         }
     }
 
