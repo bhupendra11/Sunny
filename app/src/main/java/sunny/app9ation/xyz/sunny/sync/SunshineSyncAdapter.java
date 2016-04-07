c16cd27582d6f8d5623c154db59bf384a0f6d220
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2016-04-08 01:09:45 +0530

    Change base colors for app

--- app/src/main/java/sunny/app9ation/xyz/sunny/sync/SunshineSyncAdapter.java
+++ app/src/main/java/sunny/app9ation/xyz/sunny/sync/SunshineSyncAdapter.java
@@ -427,7 +427,7 @@ public class SunshineSyncAdapter extends AbstractThreadedSyncAdapter {
                     // notifications.  Just throw in some data.
                     NotificationCompat.Builder mBuilder =
                             new NotificationCompat.Builder(getContext())
-                                    .setColor(resources.getColor(R.color.sunshine_light_blue))
+                                    .setColor(resources.getColor(R.color.colorPrimaryLight))
                                     .setSmallIcon(iconId)
                                     .setLargeIcon(largeIcon)
                                     .setContentTitle(title)
