98a341bc84b0c6e1fa1bde8f50943a3dee98e1fa
bhupendra11 <bhupendrashekhawat1108@gmail.com>
2016-04-13 22:22:35 +0530

    Redesign tablet layout using cardViews

--- app/src/main/java/sunny/app9ation/xyz/sunny/DetailActivityFragment.java
+++ app/src/main/java/sunny/app9ation/xyz/sunny/DetailActivityFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -20,6 +21,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -154,6 +156,10 @@ public class DetailActivityFragment extends Fragment  implements LoaderManager.L
                     null
             );
         }
+        ViewParent vp = getView().getParent();
+        if ( vp instanceof CardView) {
+            ((View)vp).setVisibility(View.INVISIBLE);
+        }
         return null;
     }
 
@@ -179,6 +185,10 @@ public class DetailActivityFragment extends Fragment  implements LoaderManager.L
         Log.v(LOG_TAG, "In onLoadFinished");
         if (data != null && data.moveToFirst()) {
 
+            ViewParent vp = getView().getParent();
+            if ( vp instanceof CardView ) {
+                ((View)vp).setVisibility(View.VISIBLE);
+            }
             //Read weather condition ID from cursor
             int weatherId = data.getInt(COL_WEATHER_CONDITION_ID);
 
